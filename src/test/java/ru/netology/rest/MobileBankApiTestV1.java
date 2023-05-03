bac1934508ab4cab7384e206abd14da78f57d5cc
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-05-03 10:44:36 +0300

    tests added

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,38 @@
 package ru.netology.rest;
 
+import jdk.jfr.ContentType;
 import org.junit.jupiter.api.Test;
 
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasSize;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+               .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .body("", hasSize(3))
+                .body("[1].balance", greaterThenOrEqualTo(0))
+                .body("every { it.balance >= 0}", is(true));
+
     }
+
+    @Test
+    void jsonSchemaValidator() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+
 }
