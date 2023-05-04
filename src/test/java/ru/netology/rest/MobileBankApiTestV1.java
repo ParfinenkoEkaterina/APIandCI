ee4058344e45b1cd47406ffb7fbb3b65bac47f8e
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-05-04 10:16:51 +0300

    test 3 fail

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -6,33 +6,16 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.hasSize;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
         given()
                 .baseUri("http://localhost:9999/api/v1")
-               .when()
-                .get("/demo/accounts")
-                .then()
-                .statusCode(200)
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .body("", hasSize(3))
-                .body("[1].balance", greaterThenOrEqualTo(0))
-                .body("every { it.balance >= 0}", is(true));
-
-    }
-
-    @Test
-    void jsonSchemaValidator() {
-        given()
-                .baseUri("http://localhost:9999/api/v1")
                 .when()
                 .get("/demo/accounts")
                 .then()
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+                .statusCode(200)
+        ;
     }
-
-
 }
